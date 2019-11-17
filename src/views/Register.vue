14cf3f50f9d3247f196e6ba33d4f16d9a54ac3b4
Ashish <happycoder29@gmail.com>
2019-11-17 10:49:32 -0800

    Checking in code

---
+++ src/views/Register.vue
@@ -0,0 +1,24 @@
+<template>
+
+<div class="container">
+    <br>
+    <div class="row">
+        <div class="col-md-2">
+            Left
+        </div>
+        <div class="col-md-8">
+            Mid
+        </div>
+        <div class="col-md-2">
+            Right
+        </div>
+       
+    </div>
+</div>
+    
+</template>
+<script>
+export default {
+    
+}
+</script>
